6744947729fe11ee0d81816d2213ef793d3a8756
dagrutca05 <35972804+dagrutca05@users.noreply.github.com>
2018-01-31 00:03:50 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -4,8 +4,14 @@ name1 = input()
 print("Имя второго друга :")
 name2 = input()
 
-if (len(name1)) > (len(name2)):
-	print("У друга с именем " + name1 + " имя длиннее")
+print("Имя третьего друга :")
+name3 = input()
 
-else:
-	print("У друга с именем " + name2 + " имя длиннее")
+if (len(name1)) > (len(name2)) > (len(name3)):
+  print("У друга с именем " + name1 + " имя длиннее")
+
+if (len(name2)) > (len(name1)) > (len(name3)):
+  print("У друга с именем " + name2 + " имя длиннее")
+
+if (len(name3)) > (len(name2)) > (len(name1)):
+  print("У друга с именем " + name3 + " имя длиннее")
