32faac6ba5111a25172b39c730116c6741d709f8
dagrutca05 <35972804+dagrutca05@users.noreply.github.com>
2018-01-30 23:59:45 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,11 @@
+print("Имя первого друга :")
+name1 = input()
+
+print("Имя второго друга :")
+name2 = input()
+
+if (len(name1)) > (len(name2)):
+	print("У друга с именем " + name1 + " имя длиннее")
+
+else:
+	print("У друга с именем " + name2 + " имя длиннее")
