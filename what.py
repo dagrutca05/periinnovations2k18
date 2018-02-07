21cf75d05741b06206719909bb7bf4a2a5d6edd0
dagrutca05 <35972804+dagrutca05@users.noreply.github.com>
2018-02-07 15:17:03 +0300

    Add files via upload

---
+++ what.py
@@ -0,0 +1,21 @@
+
+while True:
+  print("Напишите ваши оченки по предметам")
+  print("По физике")
+  numder =int(input())
+  print("По математикe")
+  numder +=int(input())
+  print("По географие")
+  numder +=int(input())
+  print("По биологие")
+  numder += int(input())
+  print("По химии")
+  numder +=int(input())
+  indix = (numder/3)
+  if (indix) >= 4.7 and (indix) != 5:
+    print("Вы приниты")
+  else: 
+    print("Проваливай от сюда")
+  while  3:
+    print("")
+    break
