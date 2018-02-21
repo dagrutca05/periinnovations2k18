2265ff10794a4ef56e524085855787671e96e836
dagrutca05 <35972804+dagrutca05@users.noreply.github.com>
2018-02-21 15:16:14 +0300

    Add files via upload

---
+++ 45.py
@@ -0,0 +1,30 @@
+def suma():
+	D = sum([9, 4, 5])
+	return D 
+
+
+def lena():
+	D = len([9, 4, 5])
+	return D 
+
+
+def maxi():
+	D = max([9, 4, 5])
+	return D 
+
+
+def mini():
+	D = min([9, 4, 5])
+	return D 
+
+
+def sorti(a = [3, 2, 5 ,4, 7, 1]):
+	D = sorted(a)
+	return D 
+
+
+print(suma())
+print(lena())
+print(maxi())
+print(mini())
+print(sorti())
